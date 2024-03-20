7b46f050151d295e2d411cc5f7b4786925a5951f
josuebenites <142516178+josuebenites@users.noreply.github.com>
2024-03-20 00:08:39 -0500

    Update and rename wrap_data to wrap_data.R

---
+++ wrap_data.R
@@ -0,0 +1,29 @@
+ # Data list wrapper
+data_wrap <- function(list_data, cols) {
+  library(tidyverse)
+  data_processed <- lapply(list_data, function(item) {
+    # Si el item es una lista, lo procesamos con map y luego usamos reduce
+    if(is.list(item)) {
+      item_processed <- map(item, ~ .x %>%
+                              bind_rows() %>%
+                              as_tibble() %>%
+                              select(all_of(cols)))
+      return(reduce(item_processed, full_join, by = 'var'))
+    } else {
+      # Si el item no es una lista, lo procesamos directamente
+      return(item %>%
+               bind_rows() %>%
+               as_tibble() %>%
+               select(all_of(cols)))
+    }
+  })
+  
+  # Finalmente, combinamos todos los data frames procesados en uno solo
+  return(reduce(data_processed, full_join, by = 'var'))
+}
+
+    }
+  }
+  
+  return(list(tmp_list,sub_list))
+}
