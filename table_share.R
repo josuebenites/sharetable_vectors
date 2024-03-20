0f21bc60ff98b8b4737f75da23a6cdb66df60577
josuebenites <142516178+josuebenites@users.noreply.github.com>
2024-03-19 22:49:57 -0500

    Update table_share.R

--- table_share.R
+++ table_share.R
@@ -1,3 +1,4 @@
+# Table share
 table_share <- function(data, vects, comp) {
   library(tidyverse)
   datos <- data
@@ -73,6 +74,31 @@ table_share <- function(data, vects, comp) {
           }
         }
       }
+
+
+ # Data list wrapper
+      data_wrap <- function(list_data, cols) {
+  data_processed <- lapply(list_data, function(item) {
+    # Si el item es una lista, lo procesamos con map y luego usamos reduce
+    if(is.list(item)) {
+      item_processed <- map(item, ~ .x %>%
+                              bind_rows() %>%
+                              as_tibble() %>%
+                              select(all_of(cols)))
+      return(reduce(item_processed, left_join, by = 'var'))
+    } else {
+      # Si el item no es una lista, lo procesamos directamente
+      return(item %>%
+               bind_rows() %>%
+               as_tibble() %>%
+               select(all_of(cols)))
+    }
+  })
+  
+  # Finalmente, combinamos todos los data frames procesados en uno solo
+  return(reduce(data_processed, left_join, by = 'var'))
+}
+
     }
   }
   
